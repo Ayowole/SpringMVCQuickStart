db48fb136489c55a52090c33f96386be567581ab
Ayowole <ayowole.agbedejobi@gmail.com>
2018-10-12 23:09:47 -0300

    initial commit from local machine

---
+++ src/main/java/com/ayo/springmvc/conf/AppConfiguration.java
@@ -0,0 +1,23 @@
+package com.ayo.springmvc.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.ayo.springmvc.control.HomeController;
+
+@EnableWebMvc
+@ComponentScan(basePackageClasses={HomeController.class})
+public class AppConfiguration {
+    
+    @Bean
+    public InternalResourceViewResolver internalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        
+        return resolver;
+    }
+    
+}
---
+++ src/main/java/com/ayo/springmvc/control/HomeController.java
@@ -0,0 +1,14 @@
+package com.ayo.springmvc.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    
+    @RequestMapping("/home")
+    public String index() {
+        return "home";
+    }
+    
+}
